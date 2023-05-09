efd4e0b69247f74033eaa8ffb5d8c19dd6760a6c
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2023-05-09 16:20:06 -0400

    init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const cache = createCache({ key: 'css', prepend: true });
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <CacheProvider value={cache}>
+      <ChakraProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </CacheProvider>
+  );
 }
+
+export default MyApp;
